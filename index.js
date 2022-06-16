ad1a78fd75b3b3ecc4f2e7fafb658bbc9a7134ff
Navneet0801 <nk9810328872@gmail.com>
2022-06-17 00:02:14 +0530

    animation, sound, level,etc added

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    makesound(randomChosenColor);
+    level++;
+    $("h1").html(`Level ${level}`);
 }
-let randomChosenColor = buttonColors[nextSequence()];
 
-gamePattern.push(randomChosenColor);
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    makesound(userChosenColour);
+    animatePress(userChosenColour);
+    nextSequence();
+})
 
-$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+function makesound(color){
+    let audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+}
 
-let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-audio.autoplay;
+function animatePress(currentColor){
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(function(){
+        $(`#${currentColor}`).removeClass("pressed")
+    }, 100);
+}
+
+$(document).keydown(function(){
+    if(!started){
+        nextSequence();
+        $("h1").html(`Level ${level}`);
+        started = true;
+    }
+})
