675aca424b12f8ee511cd88db1abe145679cf2be
Navneet0801 <nk9810328872@gmail.com>
2022-06-17 11:01:17 +0530

    gameover, function recall added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let level = 0;
 let started = false;
 
 function nextSequence(){
+    userClickedPattern = [];
     let randomNumber = Math.floor(Math.random()*4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -18,9 +19,9 @@ function nextSequence(){
 $(".btn").click(function(){
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
     makesound(userChosenColour);
     animatePress(userChosenColour);
-    nextSequence();
 })
 
 function makesound(color){
@@ -42,3 +43,34 @@ $(document).keydown(function(){
         started = true;
     }
 })
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success");
+
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 900);
+        }
+    }
+
+    else{
+        console.log("Wrong");
+        makesound("wrong");
+
+        $(document.body).addClass("game-over");
+        setTimeout(function(){
+        $(document.body).removeClass("game-over")
+        }, 200);
+
+        $("h1").text("Game Over, Press any key to Restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
